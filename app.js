f61cdb2fbe9db5e279e77243aa46c987ba345b7a
MiqueasAguirreANX <70060960+MiqueasAguirreANX@users.noreply.github.com>
2021-05-15 07:55:58 -0300

    config

--- app.js
+++ app.js
@@ -6,35 +6,34 @@ require('dotenv').config()
 var projectsRouter = require('./routes/projects');
 const PORT = process.env.PORT || 3050;
 var mysql = require('mysql');
-
+var config = require('./config.js')
 var app = express();
 
 var db = mysql.createConnection ({
-    host: 'us-cdbr-east-03.cleardb.com',
-    user: 'b65903fc27ee44',
-    password: '241ea035',
-    database: 'heroku_1b2580197163e73'
+    host: config.HOST,
+    user: config.USER,
+    password: config.PASSWORD,
+    database: config.DATABASE
 });
 
-db.connect((err)=>{
-    if (err) {
-        console.log("Error al conectarse a mysql");
-    }
-    console.log("Connected again!");
+// open the MySQL connection
+db.connect(error => {
+    if (error) throw error;
+    console.log("Successfully connected to the database.");
+});
+  
 
-    db.query(`CREATE TABLE projects (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(80),
-        description VARCHAR(160),
-        createdAt DATE,
-        manager VARCHAR(80),
-        assignedTo VARCHAR(80),
-        status VARCHAR(80)
-    )`, (err, result) =>{
-        if (err) console.log(err)
-        if (!err) console.log(result);
-    })
-})
+/*
+CREATE TABLE projects (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(80),
+    description VARCHAR(160),
+    createdAt DATE,
+    manager VARCHAR(80),
+    assignedTo VARCHAR(80),
+    status VARCHAR(80)
+
+*/
 global.db = db;
 
 app.use(cors())
